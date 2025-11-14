f5939a481fc40eacf61de80a6156c71c039718f9
Michal Bronicki <bronickm@tcd.ie>
2025-11-14 12:29:50 +0000

    Add dummy endpoint in inference engine

--- backend/inference_engine/src/inference_engine/main.py
+++ backend/inference_engine/src/inference_engine/main.py
@@ -1,6 +1,8 @@
-def main():
-    print("Hello from inference-engine!")
+from fastapi import FastAPI
+
+app = FastAPI()
 
 
-if __name__ == "__main__":
-    main()
+@app.get("/")
+def hello_world():
+    return {"Hello": "World"}
