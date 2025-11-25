7b69e32f61034e9e49c2e358be958dfb8885cf1e
Mukul <mukulghare9@gmail.com>
2025-11-25 21:24:11 +0000

    Add UserManagement component with controller, Added userManagement service, repo, entity Implemented Keycloak for JWT and role based access

---
+++ src/main/java/com/trinity/hermes/UserManagement/Service/UserManagementService.java
@@ -0,0 +1,15 @@
+package com.trinity.hermes.UserManagement.Service;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+@Service
+@Slf4j
+public class UserManagementService {
+
+
+}
+
+
+
+
