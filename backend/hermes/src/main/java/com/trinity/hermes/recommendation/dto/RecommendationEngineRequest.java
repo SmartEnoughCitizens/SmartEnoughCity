254419936fa2e48a026ebe97c359cfea21cd47ce
rakeshlakshmanan <ralakshm@tcd.ie>
2025-12-09 18:36:22 +0000

    Refactored packages

---
+++ backend/hermes/src/main/java/com/trinity/hermes/recommendation/dto/RecommendationEngineRequest.java
@@ -0,0 +1,20 @@
+package com.trinity.hermes.recommendation.dto;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class RecommendationEngineRequest {
+
+    private String indicatorType;
+    private LocalDateTime startDate;
+    private LocalDateTime endDate;
+    private Integer limit;
+    private String aggregationType;
+}
