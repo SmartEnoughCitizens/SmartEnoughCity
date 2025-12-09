3a44946f1608f8b5086112f013ac8872a7aaec96
Naman <naman@MacBook-Air-4.local>
2025-12-09 20:18:14 +0000

    add notification code

---
+++ backend/hermes/src/main/java/com/trinity/hermes/notification/model/enums/Channel.java
@@ -0,0 +1,7 @@
+package com.trinity.hermes.notification.model.enums;
+
+public enum Channel {
+    EMAIL,
+    NOTIFICATION,
+    EMAIL_AND_NOTIFICATION;
+}
---
+++ backend/hermes/src/main/java/com/trinity/hermes/notification/services/NotificationFacade.java
@@ -0,0 +1,54 @@
+package com.trinity.hermes.notification.services;
+
+import com.trinity.hermes.notification.dto.BackendNotificationRequestDTO;
+import com.trinity.hermes.notification.model.Notification;
+import com.trinity.hermes.notification.model.User;
+import com.trinity.hermes.notification.model.enums.Channel;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+
+import java.util.Objects;
+import java.util.Set;
+
+@Slf4j
+@Component
+@RequiredArgsConstructor
+public class NotificationFacade {
+
+    private final NotificationService notificationService;
+    private final NotificationDispatcher notificationDispatcher;
+
+    public void handleBackendNotification(BackendNotificationRequestDTO backendNotificationRequestDTO) {
+        //TODO:  Add code for schema validations that need to be performed via networkNT
+        //TODO: Add code for user retreival
+        //TODO:  fix code to have facade work better
+        User user = User.builder().build();
+        Set<Notification> notificationSet = notificationService.createNotification(user, backendNotificationRequestDTO);
+        for (Notification notification : notificationSet) {
+            if (Objects.nonNull(notification) && notification.getChannel() == Channel.EMAIL || notification.getChannel() == Channel.EMAIL_AND_NOTIFICATION) {
+                notificationDispatcher.dispatchMail(notification);
+            }
+            if (Objects.nonNull(notification) && notification.getChannel() == Channel.NOTIFICATION || notification.getChannel() == Channel.EMAIL_AND_NOTIFICATION) {
+                notificationDispatcher.dispatchSse(notification);
+            }
+
+        }
+
+
+//        Notification notification = notificationService.createNotification(
+//                user.getId(),
+//                serviceType,
+//                title,
+//                req.getMessage(),
+//                priority
+//        );
+
+
+
+
+
+    }
+
+
+}
