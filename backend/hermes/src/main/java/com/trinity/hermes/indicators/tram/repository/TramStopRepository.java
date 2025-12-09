abb51823d14f6f3a7faa9b25e76da109a4bfce27
rakeshlakshmanan <ralakshm@tcd.ie>
2025-12-09 20:09:13 +0000

    minor changes

--- backend/hermes/src/main/java/com/trinity/hermes/indicators/tram/repository/TramStopRepository.java
+++ backend/hermes/src/main/java/com/trinity/hermes/indicators/tram/repository/TramStopRepository.java
@@ -1,12 +1,12 @@
 package com.trinity.hermes.indicators.tram.repository;
 
-import com.trinity.hermes.indicators.tram.entity.LuasStop;
+import com.trinity.hermes.indicators.tram.entity.TramStop;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
 @Repository
-public interface LuasStopRepository extends JpaRepository<LuasStop, String> {
-    List<LuasStop> findByLine(String line);
+public interface TramStopRepository extends JpaRepository<TramStop, String> {
+    List<TramStop> findByLine(String line);
 }
