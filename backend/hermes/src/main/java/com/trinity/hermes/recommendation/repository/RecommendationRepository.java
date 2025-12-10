622ed16755dc0fcf528aabc0c3a0aab822b5632a
Ioannis Christofilogiannis <ichristofilogian@tuc.gr>
2025-12-10 10:44:29 +0000

    now it compiles, can be merged into main

---
+++ backend/hermes/src/main/java/com/trinity/hermes/recommendation/repository/RecommendationRepository.java
@@ -0,0 +1,10 @@
+package com.trinity.hermes.recommendation.repository;
+
+import com.trinity.hermes.recommendation.entity.Recommendation;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+//TODO: Move it to a mongo repo
+public interface RecommendationRepository extends JpaRepository<Recommendation, Long> {
+}
