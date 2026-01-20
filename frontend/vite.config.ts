57e4975d4ea70c1c84b1ad9914d36cf0d4563237
Michal Bronicki <bronickm@tcd.ie>
2026-01-20 20:03:42 +0000

    Upgrade frontend pacakges and update typescript config

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,8 +1,8 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import path from 'path';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react-swc'
+import path from 'path'
 
-// https://vitejs.dev/config/
+// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   resolve: {
@@ -19,4 +19,4 @@ export default defineConfig({
       },
     },
   },
-});
+})
