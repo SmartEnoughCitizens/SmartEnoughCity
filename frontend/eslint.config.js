4dc1584112e0b3297156aab9ccb348bdfdd03d8e
Michal Bronicki <bronickm@tcd.ie>
2026-01-20 20:34:03 +0000

    Add eslint config

---
+++ frontend/eslint.config.js
@@ -0,0 +1,64 @@
+import js from '@eslint/js'
+import globals from 'globals'
+import reactHooks from 'eslint-plugin-react-hooks'
+import reactRefresh from 'eslint-plugin-react-refresh'
+import tseslint from 'typescript-eslint'
+import { defineConfig, globalIgnores } from 'eslint/config'
+import reactX from "eslint-plugin-react-x"
+import reactDom from "eslint-plugin-react-dom"
+import jsxA11y from "eslint-plugin-jsx-a11y"
+import importPlugin from "eslint-plugin-import"
+import eslintPluginUnicorn from "eslint-plugin-unicorn"
+import eslintConfigPrettier from "eslint-config-prettier/flat"
+import pluginQuery from "@tanstack/eslint-plugin-query"
+
+export default defineConfig([
+  globalIgnores(['dist']),
+  {
+    files: ['**/*.{ts,tsx}'],
+    extends: [
+      js.configs.recommended,
+      tseslint.configs.recommendedTypeChecked,
+      reactHooks.configs.flat.recommended,
+      reactRefresh.configs.vite,
+      reactX.configs["recommended-typescript"],
+      reactDom.configs.recommended,
+      jsxA11y.flatConfigs.recommended,
+      importPlugin.flatConfigs.recommended,
+      pluginQuery.configs['flat/recommended'],
+      eslintPluginUnicorn.configs.recommended,
+      eslintConfigPrettier,
+      {
+        rules: {
+          "unicorn/filename-case": "off",
+          "unicorn/prevent-abbreviations": "off",
+
+          // TODO: Reenable the below rules
+          // They are ignored temporarily, the errors should be resolved properly
+          "@typescript-eslint/no-unsafe-argument": "off",
+          "@typescript-eslint/no-unsafe-assignment": "off",
+          "@typescript-eslint/no-unsafe-return": "off",
+          "@typescript-eslint/no-floating-promises": "off",
+          "@typescript-eslint/no-misused-promises": "off",
+          "unicorn/no-null": "off",
+          "react-x/no-array-index-key": "off",
+        },
+      },
+    ],
+    languageOptions: {
+      parserOptions: {
+        project: ["./tsconfig.node.json", "./tsconfig.app.json"],
+        tsconfigRootDir: import.meta.dirname,
+      },
+      ecmaVersion: 2020,
+      globals: globals.browser,
+    },
+    settings: {
+      'import/resolver': {
+        typescript: {
+          project: ["./tsconfig.node.json", "./tsconfig.app.json"],
+        },
+      },
+    },
+  },
+])
