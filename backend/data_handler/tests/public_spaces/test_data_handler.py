435724138a3ee20eb92aa71cc1b4ae7e03766710
Michal Bronicki <bronickm@tcd.ie>
2026-03-11 14:53:01 +0000

    Fix formatting

--- backend/data_handler/tests/public_spaces/test_data_handler.py
+++ backend/data_handler/tests/public_spaces/test_data_handler.py
@@ -9,22 +9,24 @@ from tests.utils import ANY, assert_row_count, assert_rows
 def test_save_public_spaces_to_database(db_session: Session) -> None:
     assert_row_count(db_session, "public_spaces", 0)
 
-    public_spaces = pd.DataFrame([
-        {
-            "name": "Test Cafe",
-            "type": PublicSpaceType.AMENITY,
-            "subtype": "cafe",
-            "lat": 53.35,
-            "lon": -6.26,
-        },
-        {
-            "name": None,
-            "type": PublicSpaceType.SHOP,
-            "subtype": "convenience",
-            "lat": 53.36,
-            "lon": -6.25,
-        },
-    ])
+    public_spaces = pd.DataFrame(
+        [
+            {
+                "name": "Test Cafe",
+                "type": PublicSpaceType.AMENITY,
+                "subtype": "cafe",
+                "lat": 53.35,
+                "lon": -6.26,
+            },
+            {
+                "name": None,
+                "type": PublicSpaceType.SHOP,
+                "subtype": "convenience",
+                "lat": 53.36,
+                "lon": -6.25,
+            },
+        ]
+    )
 
     save_public_spaces_to_database(public_spaces)
 
