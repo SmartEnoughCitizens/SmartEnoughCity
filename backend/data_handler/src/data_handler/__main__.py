e4b7610769b469e60b2e4c5c68badc0b9b935e23
Naman <naman@MacBook-Air-4.local>
2026-03-25 11:17:29 +0000

    luas fix

--- backend/data_handler/src/data_handler/__main__.py
+++ backend/data_handler/src/data_handler/__main__.py
@@ -23,7 +23,7 @@ from data_handler.settings.data_sources_settings import (
 from data_handler.settings.database_settings import get_db_settings
 from data_handler.train.realtime_handler import irish_rail_realtime_to_db
 from data_handler.train.static_data_handler import process_train_static_data
-from data_handler.tram.forecast_handler import luas_forecasts_to_db
+from data_handler.tram.forecast_handler import luas_forecasts_to_db, luas_stops_to_db
 from data_handler.tram.static_data_handler import process_tram_static_data
 from data_handler.urls import (
     delete_static_data,
@@ -173,6 +173,7 @@ def _run_bus_dynamic(logger: logging.Logger) -> None:
 
 def _run_tram_dynamic(logger: logging.Logger) -> None:
     logger.info("Processing tram data...")
+    luas_stops_to_db()
     luas_forecasts_to_db()
 
 
