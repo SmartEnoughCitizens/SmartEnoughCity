c594477da6c52345fe49b92fa2d984d6fe5785b5
Naman <naman@MacBook-Air-4.local>
2026-03-29 21:20:41 +0100

    fix test cases

--- backend/hermes/src/test/java/com/trinity/hermes/indicators/bus/controller/BusControllerTest.java
+++ backend/hermes/src/test/java/com/trinity/hermes/indicators/bus/controller/BusControllerTest.java
@@ -1,9 +1,7 @@
 package com.trinity.hermes.indicators.bus.controller;
 
-import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
