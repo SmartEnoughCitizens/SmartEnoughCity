250cba321535886ec7dc771947461c9bd1d325cf
rakeshlakshmanan <ralakshm@tcd.ie>
2026-04-02 13:48:32 +0100

    inference engine fix

--- backend/inference_engine/src/inference_engine/db.py
+++ backend/inference_engine/src/inference_engine/db.py
@@ -6,14 +6,10 @@ from inference_engine.settings.database_settings import get_db_settings
 
 class Base(DeclarativeBase):
     pass
-from sqlalchemy.orm import sessionmaker
 
 
 engine = create_engine(
     str(get_db_settings().dsn),
     pool_pre_ping=True,
 )
-from inference_engine.settings.db_settings import get_db_settings
-
-engine = create_engine(str(get_db_settings().dsn), pool_pre_ping=True)
 SessionLocal = sessionmaker(bind=engine, autoflush=False, autocommit=False)
